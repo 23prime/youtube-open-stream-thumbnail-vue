53f100c284330c99d7c7a3d5ace0c9f86d6e5a6c
23prime <23.prime.37@gmail.com>
2022-05-23 14:28:28 +0900

    Make title component

---
+++ src/components/Title.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <img alt="logo" src="../assets/logo.png" />
+        <p>{{ defaultText }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Title",
+    mounted() {
+        browser.runtime.sendMessage({});
+    },
+    computed: {
+        defaultText() {
+            return browser.i18n.getMessage("extName");
+        },
+    },
+};
+</script>
+
+<style scoped>
+div {
+    text-align: center;
+}
+
+p {
+    font-size: 20px;
+}
+</style>
