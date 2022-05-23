3c3afc33137079a8f8743a8f9b6bb86e05b293c9
23prime <23.prime.37@gmail.com>
2022-05-23 14:29:46 +0900

    Make open button component

---
+++ src/components/OpenButton.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="content">
+        <v-btn @click="openThumbnail">Open Thumbnail</v-btn>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Current",
+
+    data() {
+        return {
+            currentUrl: null,
+            streamId: null,
+            thumbnailUrl: [],
+        };
+    },
+
+    mounted() {
+        browser.runtime.sendMessage({});
+
+        chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+            this.currentUrl = new URL(tabs[0].url);
+            this.streamId = this.currentUrl.searchParams.get("v");
+
+            if (this.isYouTube()) {
+                this.thumbnailUrl = `https://i.ytimg.com/vi/${this.streamId}/maxresdefault.jpg`;
+            }
+        });
+    },
+
+    methods: {
+        isYouTube() {
+            return this.currentUrl.host.includes("youtube.com") && this.streamId;
+        },
+
+        openThumbnail() {
+            window.open(this.thumbnailUrl);
+        },
+    },
+};
+</script>
+
+<style scoped>
+.content {
+    text-align: center;
+}
+</style>
